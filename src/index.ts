b7970a1830e0d7ff66e1dd05c4cd8c40716ca4fb
Rahmattullah <rfkrhmttllh@gmail.com>
2023-05-08 12:22:57 +0700

    setup database and create user model

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,28 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+import mongoose from 'mongoose';
+
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-}))
+  })
+);
 
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080/');
-})
+  console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_URL = 'mongodb+srv://Rifki:rahmat011099@cluster0.vgdz2uh.mongodb.net/?retryWrites=true&w=majority'; // DB URI
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
